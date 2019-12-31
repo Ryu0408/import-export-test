4d4af309c4b1470be566d7fc7ed1b984daf45fda
Ryu0408 <fbtmdghks11@naver.com>
2020-01-01 02:45:33 +0900

    이얍

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -2,6 +2,6 @@ package test;
 
 public class test {
 	public static void main(String[] args) {
-		System.out.println("테스wweryewrywerywertwetdsfaer트");
+		System.out.println("테스wweryewwetrywerywertwetdsfaer트");
 	}
 }
